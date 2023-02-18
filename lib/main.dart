0767f9e19cb49b7e8b6b24da30998f6e7bbd14c2
Jason Elisei <1eliseijas@hdsb.ca>
2023-02-17 23:02:08 -0500

    Add 2 more pages and page selector

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,119 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+//Useful : https://fonts.google.com/icons
 
 void main() {
-  runApp(const SleepTracker());
+  runApp(const MyApp());
 }
 
-class SleepTracker extends StatelessWidget {
-  const SleepTracker({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: OverviewPage(),
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Sleep Tracker',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+        ),
+        home: const MyHomePage(),
+      ),
     );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+  
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = const OverviewPage();
+        break;
+      case 1:
+        page = const AddPage();
+        break;
+      case 2:
+        page = const StatsPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: const [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Overview'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.add_circle_outline),
+                    label: Text('Add'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.insights),
+                    label: Text('Stats'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
 
@@ -20,20 +122,80 @@ class OverviewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.amber,
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: MediaQuery.of(context).size.height,
-              width: 250.0,
-              color: Colors.lightBlueAccent,
-            )
-          ],
+    //var appState = context.watch<MyAppState>();
+
+    return ListView(
+      children: const [
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Text('Overview : '),
+        ),
+      ],
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    Key? key,
+    required this.pair,
+  }) : super(key: key);
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: pair.asPascalCase,
         ),
       ),
-    ));
+    );
+  }
+}
+
+class AddPage extends StatelessWidget {
+  const AddPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //var appState = context.watch<MyAppState>();
+    
+    return ListView(
+      children: const [
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Text('Add Sleep Activity : '),
+        ),
+      ],
+    );
+  }
+}
+
+class StatsPage extends StatelessWidget {
+  const StatsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //var appState = context.watch<MyAppState>();
+
+    return ListView(
+      children: const [
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Text('Your Statistics : '),
+        ),
+      ],
+    );
   }
 }
